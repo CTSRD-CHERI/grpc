00a0cfe683895b323de586fb32a77910249357d0
Alfredo Mazzinghi <alfredo@capabilitieslimited.co.uk>
2023-05-08 13:32:14 +0100

    Include missing headers for FreeBSD tests.

--- test/core/handshake/client_ssl.cc
+++ test/core/handshake/client_ssl.cc
@@ -24,6 +24,7 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
--- test/core/handshake/server_ssl_common.cc
+++ test/core/handshake/server_ssl_common.cc
@@ -20,6 +20,7 @@
 
 #include <arpa/inet.h>
 #include <string.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
